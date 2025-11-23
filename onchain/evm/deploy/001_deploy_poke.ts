b948253677e038a1f36ea2e78c920f6d07ea6a41
Your Name <you@example.com>
2025-11-23 12:28:11 +0100

    try deploy fail

---
+++ onchain/evm/deploy/001_deploy_poke.ts
@@ -0,0 +1,47 @@
+// we import what we need from the #rocketh alias, see ../rocketh.ts
+import {deployScript, artifacts} from '#rocketh';
+// import {createPublicClient, custom} from 'viem';
+
+export default deployScript(
+	// this allow us to define our deploy function which takes as first argument an environment object
+	// This contaisn the function provided by the modules imported in 'rocketh.ts'
+	// along with other built-in functions and the named accounts
+	async ({get, deploy, namedAccounts}) => {
+		const {deployer, admin} = namedAccounts;
+
+		await deploy('Poke', {
+			account: deployer,
+			artifact: artifacts.Poke,
+			args: [],
+		});
+	},
+	{tags: ['Poke', 'Poke_deploy']},
+	//async (env) => {
+	//	const {deployer, admin} = env.namedAccounts;
+//
+	//	// const client = env.viem.publicClient;
+//
+	//	const prefix = 'proxy:';
+	//	const deployment = await env.deployViaProxy(
+	//		'Poke',
+	//		{
+	//			account: deployer,
+	//			artifact: artifacts.Poke,
+	//			args: [prefix],
+	//		},
+	//		{
+	//			owner: admin,
+	//			linkedData: {
+	//				prefix,
+	//				admin,
+	//			},
+	//		},
+	//	);
+//
+	//	//const contract = env.viem.getContract(deployment);
+	//	//const message = await contract.read.messages([deployer]);
+	//	//console.log(message);
+	//},
+	//// execute takes as a second argument an options object where you can specify tags and dependencies
+	//{tags: ['Poke', 'Poke_deploy']},
+);
---
+++ onchain/evm/hardhat.config.ts
@@ -0,0 +1,79 @@
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import { configVariable, defineConfig } from "hardhat/config";
+import HardhatDeploy from 'hardhat-deploy';
+import {
+	addForkConfiguration,
+	addNetworksFromEnv,
+	addNetworksFromKnownList,
+} from 'hardhat-deploy/helpers';
+export default defineConfig({
+  plugins: [hardhatToolboxViemPlugin,HardhatDeploy,],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+        settings: {
+					evmVersion: 'prague',
+				},
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+					optimizer: {
+						enabled: true,
+						runs: 999999,
+					},
+					evmVersion: 'prague',
+				},
+      },
+    },
+  },
+  networks: addForkConfiguration(
+		// This add the fork configuration for chosen network
+		addForkConfiguration(
+			// this add a network config for all known chain using kebab-cases names
+			// Note that MNEMONIC_<network> (or MNEMONIC if the other is not set) will
+			// be used for account
+			// Similarly ETH_NODE_URI_<network> will be used for rpcUrl
+			// Note that if you set these env variable to have the value: "SECRET" it will be like using:
+			//  configVariable('SECRET_ETH_NODE_URI_<network>')
+			//  configVariable('SECRET_MNEMONIC_<network>')
+			addNetworksFromKnownList(
+				// this add network for each respective env var found (ETH_NODE_URI_<network>)
+				// it will also read MNEMONIC_<network> to populate the accounts
+				// And like above it will use configVariable if set to SECRET
+				addNetworksFromEnv({
+					// and you can add in your specific network here
+					default: {
+						type: 'edr-simulated',
+						chainType: 'l1',
+						//allowBlocksWithSameTimestamp: true,
+						//gasPrice: 1n,
+						//mining: {
+						//	interval: 1000,
+						//},
+						accounts: {
+							mnemonic: //'witness unfold add aunt shell monster census flush plastic tube twist machine',
+								process.env.MNEMONIC || 'test test test test test test test test test test test junk', //process.env.MNEMONIC ||
+						},
+					},
+					//sepolia: {
+					//	type: "edr-simulated",
+					//	chainType: 'l1',
+					//	accounts: {
+					//		mnemonic: 'witness unfold add aunt shell monster census flush plastic tube twist machine',
+					//			//process.env.MNEMONIC || 'test test test test test test test test test test test junk', //process.env.MNEMONIC ||
+					//	},
+					//},
+					test: {
+						type: 'edr-simulated',
+						chainType: 'l1',
+					},
+				}),
+			),
+		),
+	),
+  paths: {
+		sources: ['src'],
+	},
+});
